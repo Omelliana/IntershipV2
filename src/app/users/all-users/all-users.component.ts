2b757a262858bac95fed1d42abb370ed4d87fee4
Omelliana <azazel0400@gmail.com>
2021-03-25 23:12:23 +0300

    finished row and this hellish status bar >:C ArrGHasd

--- src/app/users/all-users/all-users.component.ts
+++ src/app/users/all-users/all-users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Renderer2} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../user';
@@ -12,10 +12,12 @@ import * as moment from 'moment';
 
 export class AllUsersComponent implements OnInit {
   users: User[] = [];
-  statuses = ['Присотановлена', 'Подписка активна', 'Заблокирован'];
+  statuses = ['Приостановлена', 'Подписка активна', 'Заблокирован'];
   url = 'https://watchlater.cloud.technokratos.com/get/array';
+  parentElement: HTMLElement[] = [];
+  // childListElement: HTMLElement = document.getElementsByClassName('null')[0] as HTMLElement;
 
-  constructor( private httpClient: HttpClient ) {
+  constructor(private httpClient: HttpClient) {
   }
 
   ngOnInit(): void {
@@ -41,10 +43,9 @@ export class AllUsersComponent implements OnInit {
     return `Баланс: ${fields.replace(',', ' ')}`;
   }
 
-  // проверка если из api опять пришло нерабочее изображение :C
-  getImage(user: User): string{
-    return /\/public\/\d.jpg/.test(user.avatar)
-      ? 'https://randomuser.me/api/portraits/men/32.jpg' : user.avatar;
+  // проверка если изображение не действительно и загрузка заглушки
+  alertError(elem: any): void{
+    elem.target.src = 'https://randomuser.me/api/portraits/men/32.jpg';
   }
 
   // отобразить время
@@ -56,6 +57,52 @@ export class AllUsersComponent implements OnInit {
     { return moment(days).locale('ru').fromNow(); }
   }
 
+  // вернуть строковый статус пользователя
   getStatusOfUser(user: User): string
-  { return this.statuses[user.status]; }
+  {
+    return this.statuses[user.status];
+  }
+
+  // установить родителя для списка и проверка надо ли скрывать предыдущий
+  setParent(element: HTMLElement, childFirst: HTMLElement, childSecond: HTMLElement): HTMLElement{
+    this.parentElement[0] = childFirst;
+    this.parentElement[1] = childSecond;
+    element = element.children.length > 1 ? element.children[1] as HTMLElement : element;
+    // проверка на уже другой открытый список
+    this.checkIfSame(element);
+    this.parentElement[2] = element;
+    for (let i = 0; i < element.children.length; i++)
+    { this.parentElement[3 + i] = element.children[i] as HTMLElement; }
+    return element;
+  }
+
+  // отобразить плашку со списком статусов
+  switchChannel(event: MouseEvent): void{
+    let el = event.target as HTMLElement;
+    if (el.parentElement && el.parentElement.parentElement)
+    {
+      el = (el.parentElement.className === 'dropdown-select') ?
+      this.setParent(el.parentElement.parentElement, el, el.parentElement) :
+      this.setParent(el.parentElement, el.children[0] as HTMLElement, el);
+      el.classList.add('shown');
+    }
+  }
+
+  // проверека двух открытых списков
+  checkIfSame(element: HTMLElement): void{
+    if (this.parentElement.length > 2)
+    {
+      if (element !== this.parentElement[2])
+      { this.parentElement[2].classList.remove('shown'); }
+    }
+  }
+
+  // проверка если пользователь нажал за приделами списка, т.е. не на саму плашку и на список
+  checkOutside(event: MouseEvent): void{
+    if (this.parentElement.length) {
+      if (!this.parentElement.includes(event.target as HTMLElement)) {
+        this.parentElement[2].classList.remove('shown');
+      }
+    }
+  }
 }
